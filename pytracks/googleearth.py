f10d2a851c0f8b2e1b4c60f82b434a7a87d53868
sah <github@nthworld.net>
2011-06-23 16:38:20 -0700

    added hr heat colors to path in google earth

--- pytracks/googleearth.py
+++ pytracks/googleearth.py
@@ -28,19 +28,20 @@ class GoogleEarth:
         print>>f, "          <tilt>33</tilt>"
         print>>f, "        </Camera>"
         print>>f, "    </gx:FlyTo>"
-        color_num = 0
-        for i in range(int(min_hr), int(max_hr), int((max_hr - min_hr) / num_hr_colors + 1)):
-            print>>f, "    <Style id=\"color" + str(color_num) + "\">"
+        for i in range(0, num_hr_colors):
+            color_hr = float(i) * (max_hr - min_hr) / float(num_hr_colors) + min_hr
+            print>>f, "    <Style id=\"color" + str(i) + "\">"
             print>>f, "      <LineStyle>"
-            print>>f, "        <color>ff" + GoogleEarth.data_point_to_color(i, min_hr, max_hr) + "</color>"
+            print>>f, "        <color>ff" + GoogleEarth.data_point_to_color(color_hr, min_hr, max_hr) + "</color>"
             print>>f, "        <width>6</width>"
             print>>f, "      </LineStyle>"
             print>>f, "    </Style>"
-            color_num += 1
 
         for i in range(0, len(track.trackpoints), 5):
             av_hr = numpy.average(track.trackpoints.hrs[i:i+5])
-            color_num = int(num_hr_colors * (av_hr - min_hr) / (max_hr - min_hr)) + 1
+            color_num = int(float(num_hr_colors) * (av_hr - min_hr) / (max_hr - min_hr)) 
+            if color_num < 0: color_num = 0
+            if color_num > num_hr_colors: color_num = num_hr_colors
             print>>f, "    <Placemark>"
             print>>f, "      <name>Absolute Extruded</name>"
             print>>f, "      <styleUrl>#color" + str(color_num) + "</styleUrl>"
@@ -58,8 +59,34 @@ class GoogleEarth:
         print>>f, "</kml>"
         f.close()
         os.spawnlp(os.P_NOWAIT, "google-earth", "google-earth", fname)
-#        for i in range(20, 40):
-#            print i, GoogleEarth.data_point_to_color(i, 20.0, 40.0)
+
+    @classmethod
+    def data_point_to_color2(cls, val, min_val, max_val):
+        GAMMA = 0.8
+        INTENSITY_MAX = 255.0
+        num_spectrum_colors = 5
+        num_bits = 255
+        spectrum_color = float(num_spectrum_colors) * (float(val) - min_val) / (max_val - min_val)
+        if spectrum_color < 1: rgb = (1.0 - spectrum_color, 0, 1)
+        elif spectrum_color < 2: rgb = (0, spectrum_color - 1.0, 1)
+        elif spectrum_color < 3: rgb = (0, 1, 3.0 - spectrum_color)
+        elif spectrum_color < 4: rgb = (spectrum_color - 3.0, 1, 0)
+        else: rgb = (1, 5.0 - spectrum_color, 0)
+
+        if spectrum_color < 1.5: factor = 0.5 + (spectrum_color / 3.0)
+        else: factor = 1.0
+        color = ""
+        for x in rgb: 
+            try:
+                if x != 0: x = round(INTENSITY_MAX * ((x * factor) ** GAMMA))
+            except ValueError as e:
+                print e
+                print rgb, factor, wave_len
+                x = 0
+            color += "%02x" % x
+        # for some reason google expects the colors as BGR, so we reverse the string here
+        return color[::-1]
+
 
     @classmethod
     def data_point_to_color(cls, val, min_val, max_val):
@@ -70,16 +97,16 @@ class GoogleEarth:
 
         wave_len = (val - min_val) / (max_val - min_val) * \
             (MAX_VISIBLE_WAVE_LEN - MIN_VISIBLE_WAVE_LEN) + MIN_VISIBLE_WAVE_LEN
-        if wave_len <= 419: factor = 0.3 + 0.7 * (wave_len - 380) / (420 - 380)
+        if wave_len <= 419: factor = 0.5 + 0.5 * (wave_len - 380) / (420 - 380)
         elif wave_len >= 420 and wave_len <= 700:  factor = 1.0
-        elif wave_len >= 701 and wave_len <= 780:  factor = 0.3 + 0.7 * (780 - wave_len) / (780 - 700)
+        elif wave_len >= 701 and wave_len <= 780:  factor = 0.5 + 0.5 * (780 - wave_len) / (780 - 700)
         else: factor = 0.0
-        if wave_len <= 439: rgb = (-(wave_len - 440) / (440 - 380), 0.0, 1.0)
-        elif wave_len <= 489: rgb = (0.0, (wave_len - 440) / (490 - 440), 1.0)
-        elif wave_len <= 509: rgb = (0.0, 1.0, -(wave_len - 510) / (510 - 490))
-        elif wave_len <= 579: rgb = ((wave_len - 510) / (580 - 510), 1.0, 0)
-        elif wave_len <= 644: rgb = (1.0, -(wave_len - 645) / (645 - 580), 0)
-        elif wave_len <= 780: rgb = (1.0, 0.0, 0.0)
+        if wave_len <= 439: rgb = (-(wave_len - 440) / (440 - 380), 0.0, 1.0) #violet
+        elif wave_len <= 489: rgb = (0.0, (wave_len - 440) / (490 - 440), 1.0) #blue
+        elif wave_len <= 509: rgb = (0.0, 1.0, -(wave_len - 510) / (510 - 490)) #green
+        elif wave_len <= 579: rgb = ((wave_len - 510) / (580 - 510), 1.0, 0) #yellow
+        elif wave_len <= 644: rgb = (1.0, -(wave_len - 645) / (645 - 580), 0) #orange
+        elif wave_len <= 780: rgb = (1.0, 0.0, 0.0) #red
         else: rgb = (0.0, 0.0, 0.0)
         color = ""
         for x in rgb: 
